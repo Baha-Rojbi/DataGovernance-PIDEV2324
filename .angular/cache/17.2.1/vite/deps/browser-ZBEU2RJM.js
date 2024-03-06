976ad4f425bfae11c3d0c30afe12e7d1a6799fe6
ALISLAIMIA <ALISLIMIA01@GMAIL.COM>
2024-03-06 15:37:26 +0100

    tags
    auto detection of sensetive data

---
+++ .angular/cache/17.2.1/vite/deps/browser-ZBEU2RJM.js
@@ -0,0 +1,51 @@
+import {
+  Animation,
+  AnimationDriver,
+  AnimationEngine,
+  AnimationRenderer,
+  AnimationRendererFactory,
+  AnimationStyleNormalizer,
+  BaseAnimationRenderer,
+  NoopAnimationDriver,
+  NoopAnimationStyleNormalizer,
+  WebAnimationsDriver,
+  WebAnimationsPlayer,
+  WebAnimationsStyleNormalizer,
+  allowPreviousPlayerStylesMerge,
+  camelCaseToDashCase,
+  containsElement,
+  createEngine,
+  getParentElement,
+  invokeQuery,
+  normalizeKeyframes,
+  validateStyleProperty,
+  validateWebAnimatableStyleProperty
+} from "./chunk-6ADG6UZG.js";
+import "./chunk-AGMJLHW4.js";
+import "./chunk-XGDTR5M7.js";
+import "./chunk-FKAFVGKM.js";
+import "./chunk-VATF2DOZ.js";
+export {
+  AnimationDriver,
+  NoopAnimationDriver,
+  Animation as ɵAnimation,
+  AnimationEngine as ɵAnimationEngine,
+  AnimationRenderer as ɵAnimationRenderer,
+  AnimationRendererFactory as ɵAnimationRendererFactory,
+  AnimationStyleNormalizer as ɵAnimationStyleNormalizer,
+  BaseAnimationRenderer as ɵBaseAnimationRenderer,
+  NoopAnimationStyleNormalizer as ɵNoopAnimationStyleNormalizer,
+  WebAnimationsDriver as ɵWebAnimationsDriver,
+  WebAnimationsPlayer as ɵWebAnimationsPlayer,
+  WebAnimationsStyleNormalizer as ɵWebAnimationsStyleNormalizer,
+  allowPreviousPlayerStylesMerge as ɵallowPreviousPlayerStylesMerge,
+  camelCaseToDashCase as ɵcamelCaseToDashCase,
+  containsElement as ɵcontainsElement,
+  createEngine as ɵcreateEngine,
+  getParentElement as ɵgetParentElement,
+  invokeQuery as ɵinvokeQuery,
+  normalizeKeyframes as ɵnormalizeKeyframes,
+  validateStyleProperty as ɵvalidateStyleProperty,
+  validateWebAnimatableStyleProperty as ɵvalidateWebAnimatableStyleProperty
+};
+//# sourceMappingURL=browser-ZBEU2RJM.js.map
