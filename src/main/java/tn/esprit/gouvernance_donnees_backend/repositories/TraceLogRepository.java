9cc6526ccafeee6e0c05e7cf6b80a79f7d9cde82
Taamallah Mohamed <93285499+mohamedTaamallah@users.noreply.github.com>
2024-04-17 17:34:58 +0200

    Delete src/main/java/tn/esprit/gouvernance_donnees_backend/repositories/TraceLogRepository.java

--- src/main/java/tn/esprit/gouvernance_donnees_backend/repositories/TraceLogRepository.java
+++
@@ -1,8 +0,0 @@
-package tn.esprit.gouvernance_donnees_backend.repositories;
-
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import tn.esprit.gouvernance_donnees_backend.entities.TraceLog;
-
-public interface TraceLogRepository extends JpaRepository<TraceLog,Long> {
-}
